2fad0ea52231cd87c6df2a2139312b1c006cd2ac
Brett Fleming <brettfleming015@gmail.com>
2021-06-24 22:47:17 -0500

    got the today weather function  working

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ $("#today").text(today.format("dddd MMMM Do YYYY"));
 
 let searchBtn = document.getElementById('searchBtn');
 let searchedCity = document.getElementById('searchedCity');
-
-
-let city = 'London';
+let cityName = document.getElementById("city");
+let todaysTemp = document.getElementById('temperature');
+let todaysWindSpeed = document.getElementById('wind-speed');
+let todayshumidity = document.getElementById('humidity');
+let city = 'New York';
 const apiKey = "e85bf845a5b6e3a8c317ab669bc8f0f9";
 let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" +  city + '&appid=' + apiKey;
+let forecastApiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey
 
-
-function searchCity() {
+function searchCityWeather() {
+    cityName.textContent = city
     fetch(apiUrl)
         .then(function(response) {
             if (!response.ok) {
@@ -19,13 +22,39 @@ function searchCity() {
             return response.json();
         })
         .then(function(data) {
+            let temp =data['main']['temp'];
+            let windSpeed = data['wind']['speed'];
+            let humidity = data['main']['humidity'];
+
+
+            todaysTemp.textContent = temp;
+            todayshumidity.textContent = humidity + '%';
+            todaysWindSpeed.textContent = windSpeed;
+            
+
+        console.log(data);
+    
+});
+} 
+function searchCityForecast() {
+    fetch(forecastApiUrl)
+        .then(function(response) {
+            if (!response.ok) {
+                throw response.json();
+            }
+            return response.json();
+        })
+        .then(function(data) {
+
+            
 
         console.log(data);
     
 });
 } 
 
-searchCity();
+searchCityWeather();
+searchCityForecast();
 
 
 
@@ -37,4 +66,5 @@ searchCity();
 
 
 
-searchBtn.addEventListener('click', searchCity);
+
+// searchBtn.addEventListener('click', searchCity);
